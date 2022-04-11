0f129cfa3fc558a43707983eea9d78a11f0e007b
Tim Wu <timothy.wu@stonybrook.edu>
2022-04-11 18:19:48 -0400

    Finished level2 tilemap, just need to update code. already have the level loadable though.

--- benchmark2/src/lungo/Scenes/Level2.ts
+++ benchmark2/src/lungo/Scenes/Level2.ts
@@ -3,6 +3,7 @@ import Debug from "../../Wolfie2D/Debug/Debug";
 import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
 import { Lungo_Color } from "../Lungo_color";
 import GameLevel from "./GameLevel";
+import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
 
 export default class Level2 extends GameLevel {
     // HOMEWORK 5 - TODO
@@ -11,25 +12,48 @@ export default class Level2 extends GameLevel {
      * 
      * Not all of these loads are needed. Decide which to remove and handle keeping resources in Level1
      */
+
+    protected trampolineIcon: Sprite;
+    protected shieldIcon: Sprite;
+
     loadScene(): void {
         // Load resources
         this.load.tilemap("level2", "lungo_assets/tilemaps/level2.json");
+        this.load.tilemap("level1", "lungo_assets/tilemaps/level1.json");
+        this.load.spritesheet("player", "lungo_assets/spritesheets/lungo.json");
+        this.load.spritesheet("shield", "lungo_assets/spritesheets/shield.json");
+        this.load.spritesheet("red", "lungo_assets/spritesheets/redBalloon.json");
+        this.load.spritesheet("blue", "lungo_assets/spritesheets/blueBalloon.json");
         this.load.spritesheet("green", "lungo_assets/spritesheets/greenBalloon.json");
+        this.load.spritesheet("basicEnemy", "lungo_assets/spritesheets/basicEnemy.json");
+        this.load.image("trampolineIcon", "lungo_assets/images/trampoline.png");
+        this.load.image("shieldIcon", "lungo_assets/images/shield.png");
+        this.load.audio("jump", "lungo_assets/sounds/jump.wav");
+        this.load.audio("switch", "lungo_assets/sounds/switch.wav");
+        this.load.audio("player_death", "lungo_assets/sounds/player_death.wav");
+        this.load.audio("pop", "lungo_assets/sounds/pop.wav")
+        // HOMEWORK 5 - TODO
+        // You'll want to change this to your level music
+        this.load.audio("level_music", "lungo_assets/music/menu.mp3");
 
     }
 
     startScene(): void {
         // Add the level 2 tilemap
         this.add.tilemap("level2", new Vec2(2, 2));
-        this.viewport.setBounds(0, 0, 64*32, 20*32);
+        this.viewport.setBounds(0, 0, 96*32, 48*32);
 
-        this.playerSpawn = new Vec2(4*32, 15*32);
+        this.playerSpawn = new Vec2(3*32, 23*32);
         this.shieldSpawn = this.playerSpawn;
-        this.totalSwitches = 7;
-        this.totalBalloons = 7;
 
         // Do generic setup for a GameLevel
         super.startScene();
+
+        this.trampolineIcon = this.add.sprite("trampolineIcon", "UI");
+        this.trampolineIcon.position.set(250, 25);
+
+        this.shieldIcon = this.add.sprite("shieldIcon", "UI");
+        this.shieldIcon.position.set(300, 25);
 
         this.addLevelEnd(new Vec2(60, 12), new Vec2(2, 2));
 
@@ -49,5 +73,20 @@ export default class Level2 extends GameLevel {
 
     updateScene(deltaT: number): void {
         super.updateScene(deltaT);
+
+        // TODO - this needs to be in every level. it feels like bad practice but shrug.
+        if (this.shieldTrampolineTimer.isStopped()) {
+            this.trampolineIcon.alpha = 1;
+        }
+        else {
+            this.trampolineIcon.alpha = 0.4;
+        }
+
+        if (this.shieldWallTimer.isStopped()) {
+            this.shieldIcon.alpha = 1;
+        }
+        else {
+            this.shieldIcon.alpha = 0.4;
+        }
     }
 }
